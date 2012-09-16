e8bde43c3b677b2542f8e915b3f3b87e1aa8f2cc
Christoph Walcher <Christoph.Walcher@gmail.com>
2012-09-16 12:32:32 +0200

    Emits better error messages

--- templates/app/helpers.js
+++ templates/app/helpers.js
@@ -1,5 +1,7 @@
 var util = require('util');
 
+var errorTag = '<span class="error help-inline">%s</span>';
+
 module.exports = function (app) {
     app.locals({
         errorFor : function (model, property) {
@@ -7,7 +9,15 @@ module.exports = function (app) {
 
             if (modelToValidate && modelToValidate.errors) {
                 if (property && modelToValidate.errors[property]) {
-                    return '<span class="error help-inline">' + modelToValidate.errors[property].message + '</span>';
+                    var error = modelToValidate.errors[property];
+
+                    if (error.type == 'required') {
+                        var fieldName = error.path[0].toLocaleUpperCase() + error.path.slice(1);
+                        var message = util.format('%s is required', fieldName);
+                        return util.format(errorTag, message);
+                    }
+
+                    return util.format(errorTag, modelToValidate.errors[property].message);
                 }
             }
         },
