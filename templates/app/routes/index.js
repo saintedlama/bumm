0605be68fa516a167f6519c238952a23e005bc4d
saintelama <christoph.walcher@gmail.com>
2012-11-26 09:31:25 +0100

    Indent by 4 spaces instead of 2

--- templates/app/routes/index.js
+++ templates/app/routes/index.js
@@ -1,11 +1,11 @@
 var path = require('path'),
-  fs = require('fs');
+    fs = require('fs');
 
-var routes = function (app) {
-  fs.readdirSync(__dirname).filter(function (file) {
-    return path.join(__dirname, file) != __filename;
-  }).forEach(function (file) {
-      require('./' + path.basename(file))(app);
+var routes = function(app) {
+    fs.readdirSync(__dirname).filter(function(file) {
+        return path.join(__dirname, file) != __filename;
+    }).forEach(function(file) {
+        require('./' + path.basename(file))(app);
     });
 }
 
