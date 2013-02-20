8fb27d1f77c2c988b6bb68780c053685b80bfb0b
saintelama <christoph.walcher@gmail.com>
2013-02-20 09:29:20 +0100

    Allow relative path for models

--- templates/app/routes/index.js
+++ templates/app/routes/index.js
@@ -1,12 +1,31 @@
 var path = require('path'),
     fs = require('fs');
 
-var routes = function(app) {
-    fs.readdirSync(__dirname).filter(function(file) {
+var requireRoutes = function(dir, app) {
+    var files = fs.readdirSync(dir).filter(function(file) {
         return path.join(__dirname, file) != __filename;
-    }).forEach(function(file) {
-        require('./' + path.basename(file))(app);
     });
+
+    files.forEach(function(file) {
+        var absolutePath = path.join(dir, file);
+
+        var stat = fs.statSync(absolutePath);
+
+        if (stat.isFile()) {
+            try {
+                require(absolutePath)(app);
+            } catch (e) {
+                console.log('Could not require route "', absolutePath, '"');
+            }
+        } else if (stat.isDirectory()) {
+            // Scan the directory recursive
+            requireRoutes(path.join(dir, file), app);
+        }
+    });
+}
+
+var routes = function(app) {
+    requireRoutes(__dirname, app);
 }
 
 module.exports = routes;
