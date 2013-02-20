7934ce2b8e5ef9cadcf2d42a04347ee53958663e
saintelama <christoph.walcher@gmail.com>
2013-02-20 10:21:08 +0100

    Add support to render a nice welcome page

--- templates/app/routes/index.js
+++ templates/app/routes/index.js
@@ -1,6 +1,8 @@
 var path = require('path'),
     fs = require('fs');
 
+var mainRoutes = [];
+
 var requireRoutes = function(dir, app) {
     var files = fs.readdirSync(dir).filter(function(file) {
         return path.join(__dirname, file) != __filename;
@@ -13,7 +15,12 @@ var requireRoutes = function(dir, app) {
 
         if (stat.isFile()) {
             try {
-                require(absolutePath)(app);
+                var defineRoutes = require(absolutePath);
+                defineRoutes(app);
+
+                if (defineRoutes.meta) {
+                    mainRoutes.push(defineRoutes.meta);
+                }
             } catch (e) {
                 console.log('Could not require route "' + absolutePath + '" due to exception', e);
             }
@@ -26,7 +33,11 @@ var requireRoutes = function(dir, app) {
 
 var routes = function(app) {
     requireRoutes(__dirname, app);
-    
+
+    // Defines the root page. can be safely removed!
+    app.get('/', function(req, res) {
+        res.render('index', { mainRoutes : mainRoutes });
+    })
 }
 
 module.exports = routes;
