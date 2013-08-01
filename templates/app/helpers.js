72f4cf4b1bf8e8da12dd08ced7bc0fe240d86097
saintelama <christoph.walcher@gmail.com>
2013-08-01 19:51:00 +0200

    Fix mongoose/express error message handling

--- templates/app/helpers.js
+++ templates/app/helpers.js
@@ -2,19 +2,12 @@ var util = require('util');
 
 var errorTag = '<span class="error help-inline">%s</span>';
 
-var getMessage = function(error) {
-    // Get type if specified or a more general message
-    return error.type || error.message;
-};
-
 module.exports = function(app) {
     app.locals({
         errorFor : function(model, property) {
-            var modelToValidate = this[model];
-
-            if (modelToValidate && modelToValidate.errors) {
-                if (property && modelToValidate.errors[property]) {
-                    var error = modelToValidate.errors[property];
+            if (model && model.errors) {
+                if (property && model.errors[property]) {
+                    var error = model.errors[property];
 
                     if (error.type == 'required') {
                         var fieldName = error.path[0].toLocaleUpperCase() + error.path.slice(1);
@@ -22,7 +15,7 @@ module.exports = function(app) {
                         return util.format(errorTag, message);
                     }
 
-                    return util.format(errorTag, getMessage(error));
+                    return util.format(errorTag, error.type || error.message);
                 }
             }
         },
